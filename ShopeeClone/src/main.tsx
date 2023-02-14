53be93a76aa4954e956a4b6598090906495f53ad
Dang Tuan Binh <tuanbinh.hehe@gmail.com>
2023-02-14 17:24:42 +0700

    CallAPI-Register

--- ShopeeClone/src/main.tsx
+++ ShopeeClone/src/main.tsx
@@ -2,11 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClientProvider } from 'react-query'
+import { QueryClient } from 'react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
