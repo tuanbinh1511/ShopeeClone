38e9f6f354ec0de6f0b1db0245e2d6be811a229d
Dang Tuan Binh <tuanbinh.hehe@gmail.com>
2023-02-15 17:51:44 +0700

    handle-Login

--- ShopeeClone/src/App.tsx
+++ ShopeeClone/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
 import useRouteElement from './useRouteElement'
-
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 function App() {
   const routeElements = useRouteElement()
 
-  return <div>{routeElements}</div>
+  return (
+    <div>
+      {routeElements}
+      <ToastContainer />
+    </div>
+  )
 }
 
 export default App
