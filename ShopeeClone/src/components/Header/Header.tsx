5f9c102278a703e19b6b30eb177accab0628c109
Dang Tuan Binh <tuanbinh.hehe@gmail.com>
2023-02-17 00:40:21 +0700

    FloatingUI-FrammerMotion-Popover

--- ShopeeClone/src/components/Header/Header.tsx
+++ ShopeeClone/src/components/Header/Header.tsx
@@ -1,11 +1,38 @@
+import { offset, shift, useFloating } from '@floating-ui/react'
+import { useInteractions } from '@floating-ui/react'
+import { useHover, arrow } from '@floating-ui/react'
+import { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { FloatingPortal } from '@floating-ui/react'
+import { motion, AnimatePresence } from 'framer-motion'
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false)
+  const arrowRef = useRef(null)
+  const { x, y, strategy, refs, context, middlewareData } = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+    middleware: [
+      offset(6),
+      shift(),
+      arrow({
+        element: arrowRef
+      })
+    ]
+  })
+  const hover = useHover(context)
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([hover])
+
   return (
     <div className='bg-[linear-gradient(-180deg,#f53d2d,#f63)] pb-5 pt-2 '>
       <div className='container'>
         <div className='flex justify-end'>
-          <div className='flex cursor-pointer items-center py-1 text-white hover:text-gray-100'>
+          <div
+            className='flex cursor-pointer items-center py-1 text-white hover:text-gray-100'
+            ref={refs.setReference}
+            {...getReferenceProps()}
+          >
             <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
@@ -27,10 +54,45 @@ function Header() {
               viewBox='0 0 24 24'
               strokeWidth={1.5}
               stroke='currentColor'
-              className='h-5 w-5'
+              className='h-6 w-6'
             >
               <path strokeLinecap='round' strokeLinejoin='round' d='M19.5 8.25l-7.5 7.5-7.5-7.5' />
             </svg>
+            <FloatingPortal>
+              <AnimatePresence>
+                {isOpen && (
+                  <motion.div
+                    ref={refs.setFloating}
+                    style={{
+                      position: strategy,
+                      top: y ?? 0,
+                      left: x ?? 0,
+                      transformOrigin: `${middlewareData.arrow?.x}px top`
+                    }}
+                    initial={{ opacity: 0, transform: 'scale(0.05)' }}
+                    animate={{ opacity: 1, transform: 'scale(1)' }}
+                    exit={{ opacity: 0, transform: 'scale(0)' }}
+                    transition={{ duration: 0.2 }}
+                    {...getFloatingProps()}
+                  >
+                    <span
+                      ref={arrowRef}
+                      style={{
+                        left: middlewareData.arrow?.x,
+                        top: middlewareData.arrow?.y
+                      }}
+                      className='absolute z-10 translate-y-[-95%] border-[11px] border-x-transparent border-t-transparent border-b-white '
+                    ></span>
+                    <div className='relative rounded-sm border-gray-100 bg-white shadow-md '>
+                      <div className='flex flex-col py-2 px-3'>
+                        <button className='py-2 px-2 hover:text-orange'>Tiếng Việt</button>
+                        <button className='py-2 px-2 hover:text-orange'>English</button>
+                      </div>
+                    </div>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </FloatingPortal>
           </div>
           <div className='ml-6 flex cursor-pointer items-center py-1 text-white hover:text-gray-100'>
             <div className='mr-2 h-6 w-6 flex-shrink-0 '>
