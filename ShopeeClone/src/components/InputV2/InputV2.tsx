243c6aa34947259dea32121c39888790c64ef86a
Dang Tuan Binh <tuanbinh.hehe@gmail.com>
2023-02-25 17:51:42 +0700

    Sử dụng useController để tạo ra InputV2

---
+++ ShopeeClone/src/components/InputV2/InputV2.tsx
@@ -0,0 +1,47 @@
+import { forwardRef, InputHTMLAttributes, useState } from 'react'
+import { useController, UseControllerProps } from 'react-hook-form'
+// eslint-disable-next-line import/no-unresolved
+import { FieldPath, FieldValues } from 'react-hook-form/dist/types'
+
+export type InputNumberProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> = {
+  classNameInput?: string
+  classNameError?: string
+} & InputHTMLAttributes<HTMLInputElement> &
+  UseControllerProps<TFieldValues, TName>
+
+function InputV2<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>(props: InputNumberProps<TFieldValues, TName>) {
+  const {
+    type,
+    onChange,
+    className,
+    classNameInput = 'p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm',
+    classNameError = 'mt-1 text-red-600 min-h-[1.25rem] text-sm',
+    value = '',
+    ...rest
+  } = props
+  const { field, fieldState } = useController(props)
+  const [localValue, setLocalValue] = useState<string>(field.value)
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const valueFormInput = event.target.value
+    const numberCondition = type === 'number' && (/^\d+$/.test(valueFormInput) || valueFormInput === '')
+    if (numberCondition || type !== 'number') {
+      setLocalValue(valueFormInput)
+      field.onChange(event)
+      onChange && onChange(event)
+    }
+  }
+  return (
+    <div className={className}>
+      <input className={classNameInput} {...rest} {...field} onChange={handleChange} value={value || localValue} />
+      <div className={classNameError}>{fieldState.error?.message}</div>
+    </div>
+  )
+}
+
+export default InputV2
